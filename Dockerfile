e460957f9b1cfb9cd0eb43c1db4e0af751c0e18e
Artem <70117318+044RoHS@users.noreply.github.com>
2025-12-05 03:13:11 +0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM gcc:13
+WORKDIR /app
+COPY test1.cpp .
+RUN g++ -o test1 test1.cpp
+CMD ["./test1"]
---
+++ test1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "? test1 работает!" << std::endl;
+    return 0;
+}
